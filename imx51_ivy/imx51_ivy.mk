592689cb2845ed718e9d6b8cf68cce63284deacc
Charlie Mu <charlie.wood.mu@gmail.com>
2011-09-23 17:55:42 +0800

    gms: remove goolge gms packages from android 3.0 build

--- imx51_ivy/imx51_ivy.mk
+++ imx51_ivy/imx51_ivy.mk
@@ -2,7 +2,7 @@
 # It will inherit from FSL core product which in turn inherit from Google generic
 
 $(call inherit-product, device/imxdev/imx5x/imx5x.mk)
-$(call inherit-product, vendor/google/products/gms_full.mk)
+#$(call inherit-product, vendor/google/products/gms_full.mk)
 
 PRODUCT_PACKAGES += \
 	glgps gps.imx5x.so			\
